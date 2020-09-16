42c3191e8b0c1cd52647ff2ab8f8425556dfe4fc
zhangjianhua <zhangjianhua@xgd.com>
2020-09-16 16:00:14 +0800

    ﻿【提交类型】: 追加提交 【问题描述】: 使用阿里云服务器 redis

--- spring-mvc/src/main/java/com/jason/web/constant/CommonConstant.java
+++ spring-mvc/src/main/java/com/jason/web/constant/CommonConstant.java
@@ -2,6 +2,6 @@ package com.jason.web.constant;
 
 public class CommonConstant {
 
-    public static String LOCK_KEY_PREFIX = "LOCK_KEY:";
+    public static String LOCK_KEY_PREFIX = "jason:lock_key:";
 
 }
