17fa2817c990bf04d8ab40b269724d6e9fef50ac
zhangjianhua <zhangjianhua@linklogis.com>
2021-06-08 15:59:20 +0800

    测试提交

---
+++ spring-mvc/src/main/java/com/jason/web/test/LyrTest.java
@@ -0,0 +1,11 @@
+package com.jason.web.test;
+
+/**
+ * @author zhangjianhua
+ * @create 2021-06-08 15:55
+ */
+public class LyrTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Lyr!!!");
+    }
+}
